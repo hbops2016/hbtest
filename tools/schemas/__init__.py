74cf4814c655a8664b479ff9db38b6d035954754
hbops2016 <142368703+hbops2016@users.noreply.github.com>
2023-08-23 13:40:42 +0800

    Delete tools directory

--- tools/schemas/__init__.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional, Any
-
-
-class ResponseSchema(BaseModel):
-    code: Optional[int] = 200
-    message: Optional[str] = "success"
-    data: Optional[Any] = None
